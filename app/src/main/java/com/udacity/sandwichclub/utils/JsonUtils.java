f4eb3db63563c4c4babda91bed49e7f384bd1f66
AbdulHakeem AbdulAleem <hakeemtunde@hotmail.com>
2018-08-03 23:33:12 +0100

    refactor JsonUtil fields name

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -13,17 +13,25 @@ import java.util.List;
 
 public class JsonUtils {
 
+    public static final String NAME = "name";
+    public static final String MAIN_NAME = "mainName";
+    public static final String DESCRIPTION = "description";
+    public static final String IMAGE = "image";
+    public static final String PLACE_OF_ORIGIN = "placeOfOrigin";
+    public static final String INGREDIENTS = "ingredients";
+    public static final String ALSO_KNOWN_AS = "alsoKnownAs";
+
     public static Sandwich parseSandwichJson(String json) {
         Sandwich sandwich = new Sandwich();
         try {
             JSONObject sandwichobj = new JSONObject(json);
-            JSONObject name = sandwichobj.getJSONObject("name");
-            sandwich.setMainName(name.getString("mainName"));
-            sandwich.setDescription(sandwichobj.getString("description"));
-            sandwich.setImage(sandwichobj.getString("image"));
-            sandwich.setPlaceOfOrigin(sandwichobj.getString("placeOfOrigin"));
-            List<String> ingredients = parseJsonArray(sandwichobj.getJSONArray("ingredients"));
-            List<String> knownas = parseJsonArray(name.getJSONArray("alsoKnownAs"));
+            JSONObject name = sandwichobj.getJSONObject(NAME);
+            sandwich.setMainName(name.getString(MAIN_NAME));
+            sandwich.setDescription(sandwichobj.getString(DESCRIPTION));
+            sandwich.setImage(sandwichobj.getString(IMAGE));
+            sandwich.setPlaceOfOrigin(sandwichobj.getString(PLACE_OF_ORIGIN));
+            List<String> ingredients = parseJsonArray(sandwichobj.getJSONArray(INGREDIENTS));
+            List<String> knownas = parseJsonArray(name.getJSONArray(ALSO_KNOWN_AS));
             sandwich.setIngredients(ingredients);
             sandwich.setAlsoKnownAs(knownas);
         } catch (JSONException e) {
